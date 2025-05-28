f2195e53caac5ff11da8ff1e54f57ebcd72d1a29
Beibars Nygyzbaev <beibarsnygyzbaev@Beibarss-MacBook-Air.local>
2025-05-27 21:17:54 -0400

    мой первый коммит, я устал

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE january_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 1;
+
+CREATE TABLE february_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 2;
+
+CREATE TABLE march_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 3;
